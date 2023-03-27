304976ec566cb8a41748b44e180323cde16d19bf
Michel Oliverio <michael.oliverio@edu.unito.it>
2023-03-27 10:52:00 +0200

    Spostato le query gia inviate nelle pull request

--- migrations/da inviare/2023-02-03_query_sommarie.sql
+++ migrations/da inviare/2023-02-03_query_sommarie.sql
@@ -15,7 +15,3 @@ HAVING
 2=2 AND `Q.tà` > 0
 ORDER BY
 `descrizione`" WHERE `name` = 'Giacenze sedi';
-
-
-INSERT INTO `zz_plugins` (`id`, `name`, `title`, `idmodule_from`, `idmodule_to`, `position`, `script`, `enabled`, `default`, `order`, `compatibility`, `version`, `options2`, `options`, `directory`, `help`, `created_at`, `updated_at`) VALUES
-(52,	'Articoli da evadere',	'Articoli da evadere',	24,	24,	'tab_main',	'',	1,	1,	0,	'2.*',	'0.1',	NULL,	' {\"main_query\": [ { \"type\": \"table\", \"fields\": \"Cliente, Numero ordine, Descrizione, Qta ordinata, Qta evasa, Qta da evadere, Data ordine\", \"query\": \"SELECT or_ordini.idstatoordine, concat(an_anagrafiche.codice, \' - \', an_anagrafiche.ragione_sociale) as Cliente, or_ordini.numero as \'Numero ordine\', or_righe_ordini.idarticolo, mg_articoli.descrizione as Descrizione, \nor_righe_ordini.qta as \'Qta ordinata\', or_righe_ordini.qta_evasa as \'Qta evasa\', (or_righe_ordini.qta - or_righe_ordini.qta_evasa) AS \'Qta da evadere\', data as \'Data ordine\' FROM or_righe_ordini INNER JOIN or_ordini ON or_ordini.id = or_righe_ordini.idordine INNER JOIN mg_articoli ON mg_articoli.id = or_righe_ordini.idarticolo INNER JOIN an_anagrafiche ON or_ordini.idanagrafica = an_anagrafiche.idanagrafica WHERE ((or_righe_ordini.qta - or_righe_ordini.qta_evasa) > 0) AND or_ordini.idstatoordine = 7 AND or_ordini.idtipoordine = 2\"} ]}\n\n',	'',	'',	'2022-12-21 14:51:14',	'2023-01-23 12:25:23');
